490d3f6c2f4f259499985ddd172d290bb869bdf1
Luana Vicente <luana.vicente@inspira.com.br>
2020-06-16 00:46:59 -0300

    Câmera gira ao invés de andar

--- godot/player.gd
+++ godot/player.gd
@@ -12,12 +12,6 @@ func _ready():
 func _physics_process(delta):
 	direction = Vector3(0,0,0)
 	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
+		rotate_y(deg2rad(90*delta))
 	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.z -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.z += 1
-	direction = direction.normalized()
-	move_and_slide(direction*speed*delta,Vector3(0,1,0))
+		rotate_y(deg2rad(-90*delta))
