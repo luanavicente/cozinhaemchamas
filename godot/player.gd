11732d64dd3e19a4b23afd9a87b1d6c29b7f3e3f
Luana Vicente <luana.vicente@inspira.com.br>
2020-06-16 00:16:21 -0300

    Cria projetinho simples no godot

---
+++ godot/player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody
+
+var speed = 10
+var direction = Vector3()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	direction = Vector3(0,0,0)
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.z -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.z += 1
+	direction = direction.normalized()
+	move_and_slide(direction*speed,Vector3(0,1,0))
