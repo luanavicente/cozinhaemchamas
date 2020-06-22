033f8b284519a86519d3967e8a8eaa77c04f3b03
Luana Vicente <luana.vicente@inspira.com.br>
2020-06-22 20:13:30 -0300

    Cria collision body pro balcão

--- godot/player.gd
+++ godot/player.gd
@@ -13,11 +13,13 @@ func _ready():
 func _physics_process(delta):
 	direction = Vector3(0,0,0)
 	if Input.is_action_pressed("ui_left"):
-		rotate_y(deg2rad(90*delta))
+		#rotate_y(deg2rad(90*delta))
+		direction.x -= 1
 	if Input.is_action_pressed("ui_right"):
-		rotate_y(deg2rad(-90*delta))
+		#rotate_y(deg2rad(-90*delta))
+		direction.x += 1
 	if Input.is_action_pressed("ui_up"):
-		direction.z += 1
+		direction.z -= 1
 	if Input.is_action_pressed("ui_down"):
-		direction.z -= 1	
+		direction.z += 1
 	move_and_slide(direction*delta*speed,Vector3(0,1,0))
